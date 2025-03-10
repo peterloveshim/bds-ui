f9ab369de2e00bf87fdd05791541992f3e5ef364
peterloveshim <peterloveshim@gmail.com>
2025-03-10 16:23:36 +0900

    first commit

---
+++ vite.config.ts
@@ -0,0 +1,62 @@
+/// <reference types="vite/client" />
+/// <reference types="vitest" />
+import path, { resolve } from 'node:path'
+import { fileURLToPath } from 'node:url'
+import { globSync } from 'node:fs'
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import dts from 'vite-plugin-dts'
+import { libInjectCss } from 'vite-plugin-lib-inject-css'
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [
+    react(),
+    libInjectCss(),
+    // A Vite plugin that generates declaration files (*.d.ts) from .ts(x)
+    dts({
+      exclude: ['**/*.stories.tsx', 'src/test', '**/*.test.tsx'], // 타입 선언 빌드 과정에서 제외
+      tsconfigPath: 'tsconfig.app.json',
+    }),
+  ],
+  build: {
+    lib: {
+      entry: resolve(__dirname, 'src/main.ts'),
+      formats: ['es'],
+    },
+    rollupOptions: {
+      external: ['react', 'react-dom', 'react/jsx-runtime'], // 외부 종속성
+      input: Object.fromEntries(
+        globSync(['src/components/**/index.tsx', 'src/main.ts']).map((file) => {
+          const entryName = path.relative(
+            'src',
+            file.slice(0, file.length - path.extname(file).length)
+          )
+          const entryUrl = fileURLToPath(new URL(file, import.meta.url))
+          return [entryName, entryUrl]
+        })
+      ),
+      output: {
+        entryFileNames: '[name].js',
+        assetFileNames: 'assets/[name][extname]',
+        globals: {
+          react: 'React',
+          'react-dom': 'React-dom',
+          'react/jsx-runtime': 'react/jsx-runtime',
+        },
+      },
+    },
+  },
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    setupFiles: './src/test/setup.ts',
+    // you might want to disable it, if you don't have tests that rely on CSS
+    // since parsing CSS is slow
+    css: true,
+    coverage: {
+      include: ['src/components'],
+      exclude: ['**/*.stories.tsx'],
+    },
+  },
+})
